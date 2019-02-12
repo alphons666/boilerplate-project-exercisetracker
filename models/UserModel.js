4aa80b8de0a56eb422287546e2726f91f86cdc22
alphons666 <39222757+alphons666@users.noreply.github.com>
2019-02-12 15:16:20 -0400

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose'),
-    { Schema } = mongoose
+    { Schema } = mongoose,
+      shortid = require('shortid')
+
+const ExerciseSchema = new Schema({
+    description: {type: String, required: true},
+    duration: {type: Number, required: true},
+    date: {type: Date, default: Date.now}
+})
+
+const UserSchema = new Schema({
+    username: { type: String, required: true},
+    shortid: { type: String, default: shortid.generate},
+    exercises: {type: [ExerciseSchema], default: []}
+})
+
+module.exports = {
+    
+}
